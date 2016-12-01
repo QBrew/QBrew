afc8d7b9aecf69a7f3f038a79723692f19622ca1
Cédric Gilon <41674@heb.be>
2016-12-01 15:45:16 +0100

    Implement function with QT lib

--- Process/process.h
+++ Process/process.h
@@ -9,8 +9,13 @@ namespace qbrew
 {
 
 bool isBrewCaskInstalled();
+
 std::pair<std::string, std::string> brewVersion();
+QPair<QString, QString> brewVersion2();
+
 std::vector<std::string> search(std::string search, bool isCask);
+QStringList search2(QString searchValue, bool isCask = true);
+
 int install(std::string package, bool cask);
 std::vector<std::string> listArgument(std::string argument);
 std::vector<std::string> list(bool isCask);
