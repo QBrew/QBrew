33030a7d50a63ee7951b8d7fd6d04938fa62ff9a
Cédric Gilon <41674@heb.be>
2017-01-07 17:58:13 +0100

    Update package list after update

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -98,7 +98,7 @@ void MainWindow::update()
     progress->setWindowModality(Qt::WindowModal);
 
     progress->setMinimumDuration(0);
-    progress->setLabelText("Save favourite");
+    progress->setLabelText("Save favourites");
     progress->setValue(0);
     progress->show();
 
@@ -128,6 +128,9 @@ void MainWindow::update()
         qbrew::updateFavourite(package);
     }
     progress->setValue(3);
+
+    packagelist_->update();
+    infoBar_->clear();
 }
 
 int MainWindow::installOrUninstallDialog(bool install)
@@ -172,7 +175,6 @@ int MainWindow::installOrUninstallDialog(bool install)
 
     }
     progress->setValue(selected.size());
-    progress->close();
 
     packagelist_->update();
     infoBar_->clear();
